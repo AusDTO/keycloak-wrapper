d622b9e7e9d4684a26158bc5f0da57c479d2d11f
Mohan Ambalavanan <mohanambalavanan@gmail.com>
2016-08-10 17:30:58 +1000

    added echo statements

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -4,14 +4,18 @@
 set -e
 
 NAME=$APP_NAME
-
+echo "App name is $NAME"
 cf api $CF_API_DEV &> /dev/null
+echo "Tyring to authenticate "
 cf auth $CF_USER_DEV $CF_PASSWORD_DEV &> /dev/null
+echo "Logged in succssfuly, trying to setting org target"
 cf target -o $CF_ORG_DEV &> /dev/null
 cf target -s $CF_SPACE_DEV &> /dev/null
-
+echo "Target set, trying to push to cf"
 cf push "$NAME"-blue &> /dev/null
 cf map-route "$NAME"-blue $CF_APP_URL_DEV -n $NAME &> /dev/null
+echo "cf deployed to blue,trying green.."
 
 cf push "$NAME"-green &> /dev/null
 cf map-route "$NAME"-green $CF_APP_URL_DEV -n $NAME &> /dev/null
+echo "cf deployed to green"
